416bea81a7ad36026c9bafae333c6e82e67aa566
Peter Hurford <peter@peterhurford.com>
2014-10-16 12:23:17 -0500

    backmerge pushes the merge for you

--- git-aliases.plugin.zsh
+++ git-aliases.plugin.zsh
@@ -51,6 +51,7 @@ backmerge() {
   cop master
   co $curr_branch
   git merge origin/master -m 'Backmerged master'
+  push
   echo 'Backmerge completed.'
 }
 ruby_backmerge() {
@@ -59,6 +60,7 @@ ruby_backmerge() {
   reset
   co $curr_branch
   git merge origin/master -m 'Backmerged master'
+  push
   echo 'Backmerge completed.  You may have to restart your local server.'
 }
 dif() {
